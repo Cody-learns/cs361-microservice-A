7b3ae29fe060809d1c86b0124379401e7d3935f4
cody <kingcody@oregonstate.edu>
2025-02-24 23:17:29 -0500

    Implemented microservice a and test client

---
+++ client.py
@@ -0,0 +1,37 @@
+import zmq
+import os
+
+# Set port below
+port = 5555
+
+# Set ZMQ context and socket connection
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect(f"tcp://localhost:{port}")
+
+# Sample payload
+payload = {
+    "data": [3, 1, 1, 2, 3, 2, 2, 3, 4, 5, 2, 1, 3, 4, 5, 3, 1, 2, 4, 3, 2, 1, 3, 3],
+    "title": "Very Awesome Histogram",
+    "xlabel": "X Values",
+    "ylabel": "Y Frequency",
+    "bins": 5
+}
+
+# Send example request
+input = input("Press enter to send the test payload...")
+socket.send_json(payload)
+
+# Recieve request
+response = socket.recv()
+
+# Check for errors
+if response.startswith(b"Error"):
+    print(response.decode())
+
+# Save and open image   
+else:
+    with open("example.png", "wb") as f:
+        f.write(response)
+        print("Histogram recieved! Opening now...")
+        os.startfile("example.png")
---
+++ microservice.py
@@ -0,0 +1,62 @@
+import zmq
+import json
+import io
+import matplotlib.pyplot as plt
+
+# Set port below
+port = 5555
+
+# Set ZMQ context and bind socket to port
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind(f"tcp://*:{port}")
+
+# Listen for request from client
+while True:
+    print("Server is listening...")
+
+    message = socket.recv().decode()
+
+    # Print confirmation of reciept to console
+    print("Request recieved!")
+
+    # Parse payload
+    try:
+        payload = json.loads(message)
+        data = payload.get("data", [])
+        title = payload.get("title", "Histogram")
+        xlabel = payload.get("xlabel", "Values")
+        ylabel = payload.get("ylabel", "Frequency")
+        bins = payload.get("bins", 10)
+
+        # Validate payload data
+        if not isinstance(data, list) or not all(isinstance(i, (int, float)) for i in data):
+            socket.send_string("Error: Invalid request, 'data' must be a list of numbers")
+            print("Response sent: Invalid data was recieved :(")
+            continue
+
+        # Create histogram
+        else:
+            plt.figure()
+            plt.hist(data, bins=bins, edgecolor='black')
+            plt.xlabel(xlabel)
+            plt.ylabel(ylabel)
+            plt.title(title)
+
+            # Store the histogram as a .png in memory
+            buffer = io.BytesIO()
+            plt.savefig(buffer, format="png")
+            plt.close()
+
+            # Send image bytes in response
+            socket.send(buffer.getvalue())
+            print("Response sent!")
+
+    except Exception as e:
+        socket.send_string(f"Error: {str(e)}")
+        print(f"Response sent: There was an error: {str(e)}")
+
+
+
+
+
